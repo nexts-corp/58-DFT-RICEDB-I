4c4c120732b9fcc65a58105504dd70f057bbfeab
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2016-06-09 15:22:08 +0700

    ให้ list ครั้งที่ประมูล เฉพาะที่มีข้อมูลในระบบ 4/58 เป็นต้นไป

--- ricedb4/apps/release/service/CompareFloorPriceService.php
+++ ricedb4/apps/release/service/CompareFloorPriceService.php
@@ -21,7 +21,7 @@ class CompareFloorPriceService extends CServiceBase implements ICompareFloorPric
     public function listsAuction() {
         $sql = "SELECT st.id, st.status, st.keyword"
                 . " FROM " . $this->ent . "\\Status st"
-                . " WHERE st.keyword like 'AU%'"
+                . " WHERE st.keyword like 'AU%' and st.active is not null"
                 . " ORDER BY st.id DESC";
         $data = $this->datacontext->getObject($sql);
         return $data;
