7304366f1fde5fc344b05320854351d04e139cc9
unknown <somchit.c@nexts-corp.com>
2016-06-15 00:22:04 +0700

    change menu

--- ricedb4/apps/home/widget/NavWidget.php
+++ ricedb4/apps/home/widget/NavWidget.php
@@ -18,42 +18,53 @@ class NavWidget extends CWidget {
     }
     public function render() {
         $loader = new CServiceLoaderImpl();
-        $apps = array("auction");
+        $apps = scandir("apps");//array("auction","industry");
         //ksort( $apps);
-        $list_apps = [];
+       // print_r( $apps);
+		$list_apps = [];
         $k=0;
+		$list_apps_keys = [];
 		$role=bindec($this->createBy = \th\co\bpg\cde\core\impl\ChangdaoEngineImpl::$_CURRENT_USER->resources[0]);
         foreach ($apps as $i => $value) {
-            $list_apps[] = $loader->load($value);
-            
-            $rts=$list_apps[$k]->routeTables;
-			$list_apps[$k]->sitemaps=false;
-          //ksort( $list_apps[$k]->routeTables);
-           $srts=[];
-           foreach ($rts as $i => $rt) {
-                if($rt->sitemap==true){
-					
-					$permin=bindec($rt->resource);
-					if(($role & $permin)>0){
-						$xx= $rt->operationDesc;
-						$srts[$xx]=$rt;
-						$list_apps[$k]->sitemaps=true;
-					}
-               }
-            }
-            ksort( $srts);
-            $list_apps[$k]->routeTables=[];
-            foreach ($srts as $i => $rt) {
-                 $list_apps[$k]->routeTables[]=$rt;
-            }
-            //print_r($srts);
-            //$list_apps[$k]->routeTables=[]
-            //$list_apps[$k]->routeTables=$srts;
-           /// print_r($list_apps[$k]->routeTables);
-            $k++;
+			if($value!=="." && $value!==".."){
+				$app_load=$loader->load($value);
+				$kk=$app_load->title;
+				$list_apps[$kk] =$app_load;
+				
+				$rts=$list_apps[$kk]->routeTables;
+				$list_apps[$kk]->sitemaps=false;
+			  //ksort( $list_apps[$k]->routeTables);
+			   $srts=[];
+			   foreach ($rts as $i => $rt) {
+					if($rt->sitemap==true){
+						
+						$permin=bindec($rt->resource);
+						if(($role & $permin)>0){
+							$xx= $rt->operationDesc;
+							$srts[$xx]=$rt;
+							$list_apps[$kk]->sitemaps=true;
+						}
+				   }
+				}
+				ksort( $srts);
+				$list_apps[$kk]->routeTables=[];
+				foreach ($srts as $i => $rt) {
+					 $list_apps[$kk]->routeTables[]=$rt;
+				}
+				//print_r($srts);
+				//$list_apps[$k]->routeTables=[]
+				//$list_apps[$k]->routeTables=$srts;
+			   /// print_r($list_apps[$k]->routeTables);
+				$k++;
+			}
         }
+		ksort($list_apps);
+		$list_apps_sort = [];
+		foreach ($list_apps as $i => $a) {
+			$list_apps_sort[]=$a;
+		}
 		
-		print_r( $this->createBy = \th\co\bpg\cde\core\impl\ChangdaoEngineImpl::$_CURRENT_USER);
+		//print_r( $this->createBy = \th\co\bpg\cde\core\impl\ChangdaoEngineImpl::$_CURRENT_USER);
         
         //exit();
       // print_r($list_apps);
@@ -61,7 +72,7 @@ class NavWidget extends CWidget {
        // $view = new CJView("nav", CJViewType::HTML_VIEW_ENGINE);
        // $view->apps = $list_apps;
         $datax=[];
-        $datax["apps"]=$list_apps;
+        $datax["apps"]=$list_apps_sort;
         $cc=$this->render2($datax, "home");
         return $cc;
     }
