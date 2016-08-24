6c315fd1c426d5f07ce188411cc45e84d341b7c7
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2016-08-24 14:03:43 +0700

    ตอนอนุมัติคลัง ทำให้สามารถอนุมัติคลังที่ไม่ขาย เพิ่มได้ด้วย

--- ricedb4/apps/tracking/service/ApproveAuctionService.php
+++ ricedb4/apps/tracking/service/ApproveAuctionService.php
@@ -17,6 +17,7 @@ class ApproveAuctionService extends CServiceBase implements IApproveAuctionServi
         $this->logger = \Logger::getLogger("root");
         $this->datacontext = new CDataContext("default");
     }
+
     public function listsAuction() {
         $sql = "select s from " . $this->ent . "\\Status s "
                 . " where s.keyword like 'AU%' and s.active is not null "
@@ -32,41 +33,53 @@ class ApproveAuctionService extends CServiceBase implements IApproveAuctionServi
 //                . " from dbo.fn_auction_info('" . $auccode . "')"
 //              . " where bidderWinner= 'Y' and (isSale is null or isSale = '') "
 //                . " order by province,associateId,wareHouseCode";
-        $sql = "select a.auctionNo,a.bidderNo,a.bidderAuctionNo,a.bidderQueue,a.bidderRound,a.bidderName, "
+        $sql = "select a.auctionNo,a.bidderNo,a.bidderAuctionNo,a.bidderQueue,a.bidderRound,a.bidderName,a.bidderWinner, "
                 . " a.bidderPrice,a.bidderFirstPrice,a.bidderLastPrice,a.RFV2, "
                 . " a.wareHouseCode,a.oweightAll, "
-                . " a.provinceId,a.province,a.associateId,a.associate,a.isSale,c.remark,c.Id as bidderPriceId "
+                . " a.provinceId,a.province,a.associateId,a.associate,a.isSale, "
+                . "a.bidderPriceNo, "
+                . "a.remarkPrice as remark "
                 . " from dbo.fn_auction_info('" . $auccode . "') a "
-                . " inner join dft_Bidder_Item b "
-                . " on b.bidder_history_id = a.bidderAuctionNo and b.silo = a.wareHouseCode and b.associateId = a.associateId "
-                . " inner join dft_Bidder_Price_Silo c "
-                . " on c.bidder_item_id = b.Id "
-                . " where a.bidderWinner= 'Y' and a.isSale = 'N' "
+                // . " inner join dft_Bidder_Item b "
+                // . " on b.bidder_history_id = a.bidderAuctionNo and b.silo = a.wareHouseCode and b.associateId = a.associateId "
+                // . " inner join dft_Bidder_Price_Silo c "
+                // . " on c.bidder_item_id = b.Id "
+                // . " where a.bidderWinner= 'Y' and (a.isSale is null or a.isSale = 'N') "
+                . " where a.bidderMaxPrice = 'Y' and a.isSale is null "
                 . " order by a.province,a.associateId,a.wareHouseCode";
         return $this->datacontext->pdoQuery($sql);
     }
 
     public function listsApprove($auccode) {
-        $sql = "select a.auctionNo,a.bidderNo,a.bidderAuctionNo,a.bidderQueue,a.bidderRound,a.bidderName,"
-                . " a.bidderPrice,a.bidderFirstPrice,a.bidderLastPrice,a.RFV2,"
-                . " a.wareHouseCode,a.oweightAll,"
-                . " a.provinceId,a.province,a.associateId,a.associate,a.isSale,c.remark,c.Id as bidderPriceId"
-                . " from dbo.fn_auction_info('" . $auccode . "') a"
-                . " inner join dft_Bidder_Item b"
-                . " on b.bidder_history_id = a.bidderAuctionNo and b.silo = a.wareHouseCode and b.associateId = a.associateId"
-                . " inner join dft_Bidder_Price_Silo c"
-                . " on c.bidder_item_id = b.Id"
-                . " where a.bidderWinner= 'Y' and isSale != 'N' "
+//        $sql = "select a.auctionNo,a.bidderNo,a.bidderAuctionNo,a.bidderQueue,a.bidderRound,a.bidderName,"
+//                . " a.bidderPrice,a.bidderFirstPrice,a.bidderLastPrice,a.RFV2,"
+//                . " a.wareHouseCode,a.oweightAll,"
+//                . " a.provinceId,a.province,a.associateId,a.associate,a.isSale,c.remark,c.Id as bidderPriceId"
+//                . " from dbo.fn_auction_info('" . $auccode . "') a"
+//                . " inner join dft_Bidder_Item b"
+//                . " on b.bidder_history_id = a.bidderAuctionNo and b.silo = a.wareHouseCode and b.associateId = a.associateId"
+//                . " inner join dft_Bidder_Price_Silo c"
+//                . " on c.bidder_item_id = b.Id"
+//                . " where a.bidderWinner= 'Y' and a.isSale  "
+//                . " order by a.province,a.associateId,a.wareHouseCode";
+        $sql = "select a.auctionNo,a.bidderNo,a.bidderAuctionNo,a.bidderQueue,a.bidderRound,a.bidderName,a.bidderWinner, "
+                . " a.bidderPrice,a.bidderFirstPrice,a.bidderLastPrice,a.RFV2, "
+                . " a.wareHouseCode,a.oweightAll, "
+                . " a.provinceId,a.province,a.associateId,a.associate,a.isSale, "
+                . "a.bidderPriceNo, "
+                . "a.remarkPrice as remark "
+                . " from dbo.fn_auction_info('" . $auccode . "') a "
+                . " where a.bidderMaxPrice = 'Y' and a.isSale is not null "
                 . " order by a.province,a.associateId,a.wareHouseCode";
         return $this->datacontext->pdoQuery($sql);
     }
 
     public function update($data, $isSale, $remark, $auccode) {
         $return = true;
+
         foreach ($data as $key => $value) {
             $bps = new \apps\common\entity\BidderPriceSilo();
-            $bps->id = $value->bidderPriceId;
-
+            $bps->id = $value->bidderPriceNo;
             $result = $this->datacontext->getObject($bps)[0];
             $result->isSale = $isSale;
             if ($remark != "-") {
@@ -76,6 +89,7 @@ class ApproveAuctionService extends CServiceBase implements IApproveAuctionServi
                 $tracking = new \apps\common\entity\RiceTracking();
                 $tracking->statusKeyword = $auccode;
                 $tracking->silo = $value->wareHouseCode;
+                $tracking->associateId = $value->associateId;
                 $dataTracking = $this->datacontext->getObject($tracking);
                 foreach ($dataTracking as $index => $valueData) {
                     $dataTracking[$index]->isSale = $isSale;
@@ -92,9 +106,9 @@ class ApproveAuctionService extends CServiceBase implements IApproveAuctionServi
     public function delete($dataAuction) {
         $return = true;
         $price = new \apps\common\entity\BidderPriceSilo();
-        $price->id = $dataAuction->bidderPriceId;
+        $price->id = $dataAuction->bidderPriceNo;
         $price = $this->datacontext->getObject($price)[0];
-        $price->isSale = 'N';
+        $price->isSale = NULL;
         $price->remark = NULL;
         if ($this->datacontext->updateObject($price)) {
             $tracking = new \apps\common\entity\RiceTracking();
@@ -118,7 +132,7 @@ class ApproveAuctionService extends CServiceBase implements IApproveAuctionServi
         $status = new \apps\common\entity\Status();
         $status->keyword = $auccode;
         $data = $this->datacontext->getObject($status)[0];
-        if ($data->active == "F" || $data->active == "FA" || $data->active == "FO" || $data->active == "FI2" ) {
+        if ($data->active == "F" || $data->active == "FA" || $data->active == "FO" || $data->active == "FI2") {
             return true;
         } else {
             return false;
