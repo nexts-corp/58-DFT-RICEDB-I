9e4d2e7fc153d5a38108d5789b2e02891b02d026
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2016-08-24 13:18:47 +0700

    แก้ search ใน bidderInfo เวลาเพิ่มผู้เสนอซื้อ
    ให้หาจากข้อมูลเดิมล่าสุดของแต่ละการประมูลของตัวเอง

--- ricedb4/apps/auction/service/BidderInfoService.php
+++ ricedb4/apps/auction/service/BidderInfoService.php
@@ -105,7 +105,7 @@ class BidderInfoService extends CServiceBase implements IBidderInfoService {
                 . " FROM " . $this->ent . "\\BidderInfo bi "
                 . "JOIN " . $this->ent . "\\BidderHistory bh WITH bh.bidderId = bi.id "
                 . " WHERE bi.taxId = :taxId "
-                . " and bh.statusKeyword not like '%-I' "
+                . " and bh.statusKeyword not like '%-I%' "
                 . " ORDER BY bh.dateCreated DESC";
         $param = array(
             "taxId" => $bidderInfo->taxId
