902b02870e88ca730dc85277edecca2aaf3245b7
Tawatchai Wisutarporn <tawatchai.w@baanprogram.com>
2017-06-05 20:27:26 +0700

    phpexcel

--- ricedb4/api.php
+++ ricedb4/api.php
@@ -4,6 +4,8 @@ date_default_timezone_set('Asia/Bangkok');
 require_once "vendor/autoload.php";
 require_once "vendor/mustache/mustache/src/Mustache/Autoloader.php";
 require_once 'vendor/bpg/cde/lib/core/AutoLoader.php';
+//const PHPEXCEL_ROOT="vendor/phpexcel";
+//require_once 'vendor/phpexcel/phpexcel/Classes/PHPExcel.php';
 use th\co\bpg\cde\core\ChangdaoEngine;
 Logger::configure(dirname(__FILE__) . '/log4php.properties');
 Mustache_Autoloader::register();
